f933f2a6ca6ec79b6fedb408feb195f3d93b5418
raducu999 <radustefan1302@gmail.com>
2021-05-08 13:55:42 +0300

    am inceput tema

--- Tema B/Tema B.R
+++ Tema B/Tema B.R
@@ -16,13 +16,35 @@ volum_elipsoid = function(a, b, c, D)
     }
   }
   
-  return (nr/D);
+  return ((nr/D)*(8*a*b*c));
 }
 
-prob = volum_elipsoid(3, 2, 4, 50000);
-sfera_statistica = prob*(8*3*2*4);
+sfera_statistica = volum_elipsoid(3, 2, 4, 50000);
 sfera_reala = ((4*pi*3*2*4)/3);
 
 print(sfera_statistica);
 print(sfera_reala);
 
+#B2
+#[0,2]*[0,4/3]
+
+arie_triunghi = function(a, b, c, d, D)
+{
+  nr = 0;
+  
+  for (i in 0: D)
+  {
+    x = runif(1, a, b);
+    y = runif(1, c, d);
+    
+    if ((x >= 0) && (y >= 0) && (y <= x) && (2*x + y <= 4))
+    {
+      nr = nr + 1;
+    }
+  }
+  
+  return ((nr/D)*((b-a)*(d-c)));
+}
+
+triunghi_statistic = arie_triunghi(0, 2, 0, 4/3, 20000);
+print(triunghi_statistic);

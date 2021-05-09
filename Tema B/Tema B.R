729664ed7f245d7a73432fa21fe6aaca761e7936
raducu999 <radustefan1302@gmail.com>
2021-05-09 11:58:37 +0300

    mai am B5 de facut

--- Tema B/Tema B.R
+++ Tema B/Tema B.R
@@ -48,3 +48,65 @@ arie_triunghi = function(a, b, c, d, D)
 
 triunghi_statistic = arie_triunghi(0, 2, 0, 4/3, 20000);
 print(triunghi_statistic);
+
+#B3
+#a)
+
+integrare_functie_1 = function(a, b, D)
+{
+  sum = 0;
+  
+  for (i in 1: D)
+  {
+    u = runif(1, a, b);
+    sum = sum + (1/(1 + sqrt(u)));
+  }
+  
+  return ((sum/D)*(b-a));
+}
+
+arie_functie_1 = integrare_functie_1(0, 1, 50000);
+print(arie_functie_1);
+
+
+#b)
+integrare_functie_2 = function(D)
+{
+  sum = 0;
+  
+  for (i in 1: D)
+  {
+    u = rexp(1, 1);
+    sum = sum + ((1/(1 + u*u))/(exp(-u)));
+  }
+  
+  return (sum/D);
+}
+
+arie_functie_2 = integrare_functie_2(50000);
+print(arie_functie_2);
+
+#B4
+
+server_request_timp = function()
+{
+  val_dist = c(rgamma(1, 5, 3), rgamma(1, 5, 2), rgamma(1, 4, 3), rgamma(1, 6, 4));
+  
+  prob_dist = c(0.35, 0.15, 0.20, 0.30);
+  
+  medie = 0;
+  
+  for (i in 1: 4)
+  {
+    medie = medie + prob_dist[c(i)]*val_dist[c(i)];
+  }
+  
+  latenta = rexp(1, 4);
+  
+  return(medie + latenta);
+}
+
+server_request_timp_val = server_request_timp();
+print(server_request_timp_val);
+
+#B5
